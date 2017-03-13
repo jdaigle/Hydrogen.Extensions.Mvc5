553bc972173ed645e26d7380432f757f37285e76
Joseph Daigle <joseph.daigle@gmail.com>
2017-03-13 14:42:16 -0400

    creating a test project with passing tests from "System.Web.Mvc.Test"

---
+++ MvcAsync.Test/MockAsyncResult.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace MvcAsync
+{
+    public class MockAsyncResult : IAsyncResult
+    {
+        private volatile object _asyncState;
+        private volatile ManualResetEvent _asyncWaitHandle = new ManualResetEvent(false);
+        private volatile bool _completedSynchronously;
+        private volatile bool _isCompleted;
+
+        public object AsyncState
+        {
+            get { return _asyncState; }
+            set { _asyncState = value; }
+        }
+
+        public ManualResetEvent AsyncWaitHandle
+        {
+            get { return _asyncWaitHandle; }
+            set { _asyncWaitHandle = value; }
+        }
+
+        public bool CompletedSynchronously
+        {
+            get { return _completedSynchronously; }
+            set { _completedSynchronously = value; }
+        }
+
+        public bool IsCompleted
+        {
+            get { return _isCompleted; }
+            set { _isCompleted = value; }
+        }
+
+        WaitHandle IAsyncResult.AsyncWaitHandle
+        {
+            get { return _asyncWaitHandle; }
+        }
+    }
+}
